7532b7a44c1ff43eda0f733abe454939790b3c10
mrhaffner <mrhaffner13@gmail.com>
2021-11-07 17:43:30 -0600

    refactor: ProjectList now iterates over new project-details.json file and image array to render FlipCards

--- components/FlipCard.tsx
+++ components/FlipCard.tsx
@@ -1,18 +1,23 @@
 import Image from 'next/image';
+import { ProjectDetail } from '../types';
 
 interface Props {
   image: StaticImageData;
-  alt: string;
+  projectDetail: ProjectDetail;
 }
 
-const FlipCard = ({ image, alt }: Props) => {
+const FlipCard = ({ image, projectDetail }: Props) => {
   return (
     <div className="card flex-1/3 w-1/3">
       <div className="card-side front w-full h-full">
-        <Image src={image} alt={alt} layout="responsive" />
+        <Image
+          src={image}
+          alt={`${projectDetail.title} + webpage`}
+          layout="responsive"
+        />
       </div>
       <div className="card-side back w-full h-full">
-        <div>Back Side</div>
+        <div>{projectDetail.title}</div>
       </div>
     </div>
   );
--- components/ProjectList.tsx
+++ components/ProjectList.tsx
@@ -1,54 +1,25 @@
-import Image from 'next/image';
 import forecastImage from '../public/images/project-list/forecast-your-snow.jpeg';
 import personalBlog from '../public/images/project-list/personal-blog.jpeg';
 import trelloClone from '../public/images/project-list/trello-clone.jpeg';
 import onTheSnowClone from '../public/images/project-list/on-the-snow-clone.jpeg';
+import projectDetails from '../public/project-details.json';
 import FlipCard from './FlipCard';
+import { ProjectDetail } from '../types';
 //background color for empty boxes - not white?
 //i want these guys to spin and then reveal project details
 //add breakpoints
 
 const ProjectList = () => {
+  const imageArray = [forecastImage, trelloClone, personalBlog, onTheSnowClone];
   return (
     <div className="w-full h-full flex flex-wrap justify-center flip-card">
-      <FlipCard
-        image={forecastImage}
-        alt="Forecast Your Snow webpage project"
-      />
-      <FlipCard image={trelloClone} alt="Trello Clone webpage project" />
-      <FlipCard image={personalBlog} alt="Personal Blog webpage project" />
-      <FlipCard
-        image={onTheSnowClone}
-        alt="On The Snow Clone webpage project"
-      />
-      {/* <div className="flex-1/3 w-full h-full">
-        <Image
-          src={forecastImage}
-          alt="Forecast Your Snow webpage project"
-          layout="responsive"
+      {projectDetails.data.map((detail: ProjectDetail, i: number) => (
+        <FlipCard
+          image={imageArray[i]}
+          projectDetail={detail}
+          key={i + Math.random()}
         />
-      </div>
-      <div className="flex-1/3 w-full h-full">
-        <Image
-          src={trelloClone}
-          alt="Forecast Your Snow webpage project"
-          layout="responsive"
-        />
-      </div>
-      <div className="flex-1/3 w-full h-full">
-        <Image
-          src={personalBlog}
-          alt="Forecast Your Snow webpage project"
-          layout="responsive"
-        />
-      </div>
-      <div className="flex-1/3 w-full h-full">
-        <Image
-          src={onTheSnowClone}
-          alt="Forecast Your Snow webpage project"
-          layout="responsive"
-        />
-      </div> */}
+      ))}
     </div>
   );
 };
