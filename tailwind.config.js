b903c2ce7f56c18e7d55ddb97e2b3c90dbd42960
mrhaffner <mrhaffner13@gmail.com>
2021-11-09 18:06:27 -0600

    style: added custom theme colors to tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,16 @@ module.exports = {
       width: {
         240: '60rem',
       },
+      colors: {
+        'midnight-blue': rgb(4, 0, 63),
+        'bright-sky-blue': rgb(0, 185, 254),
+        'hover-sky-blue': rgb(82, 206, 252),
+        mint: rgb(0, 185, 254),
+        'light-slate-blue': rgb(55, 72, 134),
+        'dark-slate-blue': rgb(40, 56, 111),
+        'gradient-pink': rgb(235, 56, 166),
+        'gradient-light-pink': rgb(245, 89, 185),
+      },
     },
   },
   variants: {
