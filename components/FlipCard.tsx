fa91b6ed0c0a3af3f8ae4603ce07ff7c6cb1cd42
mrhaffner <mrhaffner13@gmail.com>
2021-11-10 20:12:02 -0600

    feat: added link buttons to FlipCard

--- components/FlipCard.tsx
+++ components/FlipCard.tsx
@@ -1,5 +1,7 @@
 import Image from 'next/image';
+import Link from 'next/link';
 import { ProjectDetail } from '../types';
+import GithubLogo from './logos/GithubLogo';
 
 interface Props {
   image: StaticImageData;
@@ -7,6 +9,7 @@ interface Props {
 }
 
 const FlipCard = ({ image, project }: Props) => {
+  const octoCatSize = 25;
   return (
     <div className="card w-full md:flex-1/2 md:w-1/2 lg:flex-1/3 lg:w-1/3 text-midnightBlue">
       <div className="card-side front w-full h-full">
@@ -20,6 +23,25 @@ const FlipCard = ({ image, project }: Props) => {
         <h3 className="text-2xl font-bold uppercase tracking-wider">
           {project.title}
         </h3>
+        <div className="flex text-midnightBlue text-xl my-3 space-x-4">
+          {project?.githubFront && (
+            <Link href={project.githubFront}>
+              <a className="flex space-x-1">
+                <GithubLogo height={octoCatSize} width={octoCatSize} />
+                <span>Front End</span>
+              </a>
+            </Link>
+          )}
+          {project?.githubBack && (
+            <Link href={project.githubBack}>
+              <a className="flex space-x-1">
+                <GithubLogo height={octoCatSize} width={octoCatSize} />
+                <span>Back End</span>
+              </a>
+            </Link>
+          )}
+        </div>
+
         <div className="flex flex-wrap justify-center w-2/3 space-x-2">
           {project.tags.map((tag: string) => (
             <p className="py-1 px-3 mb-2 border-2 border-midnightBlue rounded-full font-medium">
