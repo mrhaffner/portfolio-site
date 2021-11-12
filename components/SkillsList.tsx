4aa92c9d4b46668cb940b58cac076e05b2fad6a8
mrhaffner <mrhaffner13@gmail.com>
2021-11-11 18:15:40 -0600

    refactor: moved LogoContainer wrapper and any accompanying text to inside individual logos to improve readabiltiy

--- components/SkillsList.tsx
+++ components/SkillsList.tsx
@@ -16,48 +16,22 @@ const SkillsList = () => {
   return (
     <div className="w-full flex flex-col justify-center items-center text-brightSkyBlue">
       <div className="w-full lg:w-240 flex flex-wrap justify-center font-semibold select-none">
-        <LogoContainer>
-          <TypescriptLogo height={maxSize / 4.096} width={maxSize} />
-        </LogoContainer>
-        <LogoContainer>
-          <PythonLogo height={maxSize} width={maxSize} />
-        </LogoContainer>
-        <LogoContainer>
-          <>
-            <DatabaseLogo height={maxSize / 3} width={maxSize / 3} />
-            <span className="text-4xl tracking-wide ml-1">SQL</span>
-          </>
-        </LogoContainer>
-        <LogoContainer>
-          <NodeLogo height={maxSize} width={maxSize} />
-        </LogoContainer>
-        <LogoContainer>
-          <ReactLogo height={maxSize / 2} width={maxSize} />
-        </LogoContainer>
-        <LogoContainer>
-          <MongoLogo height={maxSize / 2.1747499} width={maxSize} />
-        </LogoContainer>
-        <LogoContainer>
-          <>
-            <CssLogo
-              colorSecondary={colorSecondary}
-              height={maxSize / 3}
-              width={maxSize / 3}
-            />
-            <p className="text-4xl ml-1">CSS</p>
-            <span className="opacity-0">SSSS</span>
-          </>
-        </LogoContainer>
-        <LogoContainer>
-          <>
-            <HtmlLogo
-              colorSecondary={colorSecondary}
-              height={maxSize / 3}
-              width={maxSize / 3}
-            />
-            <p className="text-4xl ml-1">HTML</p>
-          </>
-        </LogoContainer>
+        <TypescriptLogo height={maxSize / 4.096} width={maxSize} />
+        <PythonLogo height={maxSize} width={maxSize} />
+        <DatabaseLogo height={maxSize / 3} width={maxSize / 3} />
+        <NodeLogo height={maxSize} width={maxSize} />
+        <ReactLogo height={maxSize / 2} width={maxSize} />
+        <MongoLogo height={maxSize / 2.1747499} width={maxSize} />
+        <CssLogo
+          colorSecondary={colorSecondary}
+          height={maxSize / 3}
+          width={maxSize / 3}
+        />
+        <HtmlLogo
+          colorSecondary={colorSecondary}
+          height={maxSize / 3}
+          width={maxSize / 3}
+        />
       </div>
     </div>
   );
--- components/logos/DatabaseLogo.tsx
+++ components/logos/DatabaseLogo.tsx
@@ -1,20 +1,27 @@
+import LogoContainer from './LogoContainer';
+
 interface Props {
   height: number;
   width: number;
 }
 
 const DatabaseLogo = ({ width, height }: Props) => (
-  <svg
-    xmlns="http://www.w3.org/2000/svg"
-    width={width}
-    height={height}
-    className="fill-current"
-    viewBox="0 0 20 20"
-  >
-    <path d="M3 12v3c0 1.657 3.134 3 7 3s7-1.343 7-3v-3c0 1.657-3.134 3-7 3s-7-1.343-7-3z" />
-    <path d="M3 7v3c0 1.657 3.134 3 7 3s7-1.343 7-3V7c0 1.657-3.134 3-7 3S3 8.657 3 7z" />
-    <path d="M17 5c0 1.657-3.134 3-7 3S3 6.657 3 5s3.134-3 7-3 7 1.343 7 3z" />
-  </svg>
+  <LogoContainer>
+    <>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width={width}
+        height={height}
+        className="fill-current"
+        viewBox="0 0 20 20"
+      >
+        <path d="M3 12v3c0 1.657 3.134 3 7 3s7-1.343 7-3v-3c0 1.657-3.134 3-7 3s-7-1.343-7-3z" />
+        <path d="M3 7v3c0 1.657 3.134 3 7 3s7-1.343 7-3V7c0 1.657-3.134 3-7 3S3 8.657 3 7z" />
+        <path d="M17 5c0 1.657-3.134 3-7 3S3 6.657 3 5s3.134-3 7-3 7 1.343 7 3z" />
+      </svg>
+      <span className="text-4xl tracking-wide ml-1">SQL</span>
+    </>
+  </LogoContainer>
 );
 
 export default DatabaseLogo;
