36d5b80a29b3bf607d369c45cac4849891ed0384
mrhaffner <mrhaffner13@gmail.com>
2021-11-15 18:45:37 -0600

    style: added mobile breakpoints to NavBar, added non-functional hamburger menu

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -1,35 +1,58 @@
 import Link from 'next/link';
+import { useEffect, useState } from 'react';
+import useWindowDimensions from '../hooks/useWindowDimensions';
+import HamburgerLogo from './svgs/HamburgerLogo';
 
-const NavBar = () => (
-  //breakpoints
-  //mobile menu
-  <nav className="w-full h-20 flex justify-between items-center px-10 xl:px-20 2xl:px-28 text-darkSlateBlue tracking-wide font-bold xl:text-lg">
-    {/* perhaps a better logo */}
-    <Link href="/">
-      <a className="font-arizonia text-3xl">
-        Matt<span className="text-gradientPink font-black">.</span>Haffner
-        <span className="text-gradientPink font-black">.</span>
-      </a>
-    </Link>
-    <ul className="flex space-x-8 uppercase">
-      <li>
-        <Link href="#skills">
-          <a className="mint-underline">Skills</a>
-        </Link>
-      </li>
-      <li>
-        <Link href="#projects">
-          <a className="mint-underline">Projects</a>
-        </Link>
-      </li>
-      {/* figure out what to do for this */}
-      <li>
-        <Link href="">
-          <a className="mint-underline">Contact</a>
-        </Link>
-      </li>
-    </ul>
-  </nav>
-);
+const NavBar = () => {
+  const [showBurger, setShowBurger] = useState(true);
+  const { width } = useWindowDimensions();
+
+  useEffect(() => {
+    if (width >= 648) {
+      setShowBurger(false);
+    } else {
+      setShowBurger(true);
+    }
+  }, [width]);
+
+  return (
+    //breakpoints
+    //mobile menu
+    <nav className="w-full h-20 flex justify-between items-center px-10 xl:px-20 2xl:px-28 text-darkSlateBlue tracking-wide font-bold xl:text-lg">
+      {/* perhaps a better logo */}
+      <Link href="/">
+        <a className="font-arizonia text-2xl md:text-3xl pb-2 md:pb-0">
+          Matt<span className="text-gradientPink font-black">.</span>Haffner
+          <span className="text-gradientPink font-black">.</span>
+        </a>
+      </Link>
+      {showBurger && (
+        <div className="">
+          <HamburgerLogo size={24} />
+        </div>
+      )}
+      {!showBurger && (
+        <ul className="flex space-x-8 uppercase">
+          <li>
+            <Link href="#skills">
+              <a className="mint-underline">Skills</a>
+            </Link>
+          </li>
+          <li>
+            <Link href="#projects">
+              <a className="mint-underline">Projects</a>
+            </Link>
+          </li>
+          {/* figure out what to do for this */}
+          <li>
+            <Link href="">
+              <a className="mint-underline">Contact</a>
+            </Link>
+          </li>
+        </ul>
+      )}
+    </nav>
+  );
+};
 
 export default NavBar;
---
+++ components/svgs/HamburgerLogo.tsx
@@ -0,0 +1,23 @@
+interface Props {
+  size: number;
+}
+
+const HamburgerLogo = ({ size }: Props) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    fill="none"
+    viewBox="0 0 24 24"
+    stroke="currentColor"
+    height={size}
+    width={size}
+  >
+    <path
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      strokeWidth={2}
+      d="M4 6h16M4 12h16M4 18h16"
+    />
+  </svg>
+);
+
+export default HamburgerLogo;
