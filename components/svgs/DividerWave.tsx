1ca4d10b2d15343ad7316879aa43f30ee1971159
mrhaffner <mrhaffner13@gmail.com>
2021-11-18 17:48:26 -0600

    fix: updated DividerWave properties to be proper JSX

--- components/svgs/DividerWave.tsx
+++ components/svgs/DividerWave.tsx
@@ -10,8 +10,8 @@ const DividerWave = () => (
     <path
       d="M0 86L50 99.8C100 113.7 200 141.3 300 135C400 128.7 500 88.3 600 76.7C700 65 800 82 850 90.5L900 99L900 201L850 201C800 201 700 201 600 201C500 201 400 201 300 201C200 201 100 201 50 201L0 201Z"
       fill="#4fe2c1"
-      stroke-linecap="round"
-      stroke-linejoin="miter"
+      strokeLinecap="round"
+      strokeLinejoin="miter"
     ></path>
   </svg>
 );
